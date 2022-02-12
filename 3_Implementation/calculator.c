e0ebfda4f1d8f871d7d850318610d35c2621ebd4
Naveenan-25 <98873140+Naveenan-25@users.noreply.github.com>
2022-02-12 17:28:17 +0530

    Update calculator.c

--- 3_Implementation/calculator.c
+++ 3_Implementation/calculator.c
@@ -1,20 +1,8 @@
-#include <stdio.h>
 #include "calculator.h"
-
-void addition();
-void subraction();
-void multiplication();
-void division();
-void modulus();
-int factorial();
-void power();
-void squareroot();
-void logvalue();
-
-
 int main()
 {
     int s;
+    int fact=1;
     start:
     printf(" \t\t\t\tWelcome to Calculator!!!!! ");
     printf("\n1.Addition \n2.Subraction \n3.Multiplication \n4.Division \n5.Modulus  \n6.Factorial \n7.Power \n8.Squareroot \n9.Log");
@@ -26,54 +14,104 @@ int main()
         switch(s){
             case 1:
             { 
-                addition();
+            float a,b,c;
+            printf("\nenter the first number:");
+            scanf("%f", &a);
+            printf("\nenter the second number:");
+            scanf("%f", &b);
+            c=a+b;
+            printf("\nThe addition is: %f ", c);
             break;
             }
-             
+            
             case 2:
             {
-               subraction();
+            float a,b,c;
+            printf("\nenter the first number:");
+            scanf("%f", &a);
+            printf("\nenter the second number:");
+            scanf("%f", &b);
+            c=a-b;
+            printf("\nThe subraction is: %f ", c);
             break;
             }
             
             case 3:
             {
-               multiplication();
+            float a,b,c;
+            printf("\nenter the first number:");
+            scanf("%f", &a);
+            printf("\nenter the second number:");
+            scanf("%f", &b);
+            c=a*b;
+            printf("\nThe multiplication is: %f ", c);
             break;
             }
             
             case 4:
             {
-            division();
+            float a,b,c;
+            printf("\nenter the first number:");
+            scanf("%f", &a);
+            printf("\nenter the second number:");
+            scanf("%f", &b);
+            c=a/b;
+            printf("\nThe division is: %f", c);
+            break;
             }
             
             case 5:
             {
-            modulus();
+            int a,b,c;
+            printf("\nenter the first number:");
+            scanf("%d", &a);
+            printf("\nenter the second number:");
+            scanf("%d", &b);
+            c=a%b;
+            printf("\nThe modulus is: %d ", c);
+            break;
             }
             
             case 6:
             {
-            factorial();
+            int num;
+            printf("\nenter the first number:");
+            scanf("%d", &num);
+            for(int i=1;i<=num;i++)
+            {
+                fact=fact*i;
+            }
+            printf("\nThe factorial is: %d ", fact);
             break;
             }
             
             case 7:
             {
-            power();
-            break;
+            float a,pnum,c;
+            printf("\nenter the number:");
+            scanf("%1f", &a);
+            printf("\nenter the power:");
+            scanf("%1f", &pnum);
+            c=pow(a,pnum);
+            printf("\nThe power is:%1f ", c);
             }
             
             case 8:
             {
-            squareroot();
-            break;
+            float num,squareroot;
+            printf("\nenter the number:");
+            scanf("%f", &num);
+            squareroot=sqrt(num);
+            printf("\nThe squareroot is: %f", squareroot);
             }
             
             case 9:
             {
-           logvalue();
-           break;
+            float number, result;
+            printf("\nenter the number:");
+            scanf("%f", &number);
+            result=log(number);
+            printf("\nThe log value is: %f", result);
             }
         }
     }
@@ -83,101 +121,4 @@ int main()
         printf("\nYou have entered the wrong choice");
         goto start;
     }
-}
-void addition()
-{
-    float a,b,c;
-            printf("\nenter the first number:");
-            scanf("%f", &a);
-            printf("\nenter the second number:");
-            scanf("%f", &b);
-            c=a+b;
-            printf("\nThe addition is: %f ", c);
-}
-
-void subraction()
-
-{
-    float a,b,c;
-            printf("\nenter the first number:");
-            scanf("%f", &a);
-            printf("\nenter the second number:");
-            scanf("%f", &b);
-            c=a-b;
-            printf("\nThe subraction is: %f ", c);
-}
- 
-void multiplication()
-{
-    float a,b,c;
-            printf("\nenter the first number:");
-            scanf("%f", &a);
-            printf("\nenter the second number:");
-            scanf("%f", &b);
-            c=a*b;
-            printf("\nThe multiplication is: %f ", c);
-    
-}
-
-void division()
-{
-     float a,b,c;
-            printf("\nenter the first number:");
-            scanf("%f", &a);
-            printf("\nenter the second number:");
-            scanf("%f", &b);
-            c=a/b;
-            printf("\nThe division is: %f", c);
-}
-
-void modulus()
-{
-    int a,b,c;
-            printf("\nenter the first number:");
-            scanf("%d", &a);
-            printf("\nenter the second number:");
-            scanf("%d", &b);
-            c=a%b;
-            printf("\nThe modulus is: %d ", c);
-            
-}
-
-int factorial()
-{
-    int num, fact=1; 
-            printf("\nenter the first number:");
-            scanf("%d", &num);
-            for(int i=1;i<=num;i++)
-            {
-                fact=fact*i;
-            }
-            printf("\nThe factorial is: %d ", fact);
-}
-
-void power()
-{
-    float a,pnum,c;
-            printf("\nenter the number:");
-            scanf("%1f", &a);
-            printf("\nenter the power:");
-            scanf("%1f", &pnum);
-            c=pow(a,pnum);
-            printf("\nThe power is:%1f ", c);
-}
-void squareroot()
-{
-    float num,squareroot;
-            printf("\nenter the number:");
-            scanf("%f", &num);
-            squareroot=sqrt(num);
-            printf("\nThe squareroot is: %f", squareroot);
-}
-
-void logvalue()
-{
-     float number, result;
-            printf("\nenter the number:");
-            scanf("%f", &number);
-            result=log(number);
-            printf("\nThe log value is: %f", result);
 }

476c8ea2be4fe7cb990e1f051ce1ef34889ac3cc
Naveenan-25 <98873140+Naveenan-25@users.noreply.github.com>
2022-02-12 16:16:45 +0530

    Update calculator.h

--- 3_Implementation/calculator.h
+++ 3_Implementation/calculator.h
@@ -1,5 +1,17 @@
+//Headers
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+
+//Functions
+void addition();
+void subraction();
+void multiplication();
+void division();
+void modulus();
+int factorial();
+void power();
+void squareroot();
+void logvalue()
 
 
